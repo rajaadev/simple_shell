f97e2d4b1058c9523e2cfe214ebcccdf53895750
Raja El Morabet <rajaa.elmorabet@gmail.com>
2024-03-01 09:53:59 +0100

    add file_string_function4.c

--- file_string_functions3.c
+++ file_string_functions3.c
@@ -1,1 +1,84 @@
 #include "shell.h"
+
+char *_strncpy(char *, char *, int);
+char *_strncat(char *, char *, int);
+char *_strchr(char *, char);
+***
+/**
+ * _strncpy - copy a string
+ * @dest: Destination
+ * @src: Source string
+ * @n: number of characters to copy
+
+ *
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return dest;
+}
+/**
+ * _strncat - concatenate two strings
+ * using at most n bytes from src
+ * @dest: Destination
+ * @src: Source
+ * @n: number of characters to concatenate
+ *
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0;
+int j = 0;
+
+
+while (dest[i] != '\0')
+{
+i++;
+}
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
+}
+
+/**
+ * _strchr - Function that locate character in string
+ * @str: string to search
+ * @c: character to find
+ * Return: point the first occurrence of the character c in the string
+ * 	or NULL if the character is not found
+ */
+char *_strchr(char *str, char c)
+{
+int i = 0;
+/*read the string include NULL */
+while (str[i] != '\0')
+    {
+/* verify if the character c is in string */
+   if (str[i] == c)
+            return &str[i];
+        i++;
+    }
+/* if c not founded in str*/
+
+return NULL;
+}
+
---
+++ file_string_functions4.c
@@ -0,0 +1,101 @@
+#include "shell.h"
+
+/**
+ * **strtow - splits a string into words. Repeat delimiters are ignored
+ * @st: string
+ * @del: delimeter 
+ * Return: pointer to array of strings, or NULL on failure
+ */
+
+char **strtow(char *st, char *del)
+{
+	int i, j, k, m, num_words = 0;
+	char **s;
+
+	if (st == NULL || st[0] == '\0')
+		return (NULL);
+	if (!del)
+		del= " ";
+	for (i = 0; st[i] != '\0'; i++)
+		if (!is_delim(st[i], del) && (is_delim(st[i + 1], del) || !st[i + 1]))
+			num_words++;
+
+	if (num_words == 0)
+		return (NULL);
+	s = malloc(( num_words +1) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < num_words; j++)
+	{
+		while (is_delim(st[i], del))
+			i++;
+		k = 0;
+		while (!is_delim(st[i + k], del) && st[i + k])
+			k++;
+
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)
+			s[j][m] = st[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
+
+/**
+ * **strtow2 - splits a string into words
+ * @str: the string
+ * @del: the delimeter
+ * Return: a pointer to an array of strings, or NULL on failure
+ */
+
+char **strtow2(char *str, char del)
+{
+	int i, j, k, m, num_words = 0;
+	char **s;
+
+
+
+	if (str == NULL || str[0] == 0)
+		return (NULL);
+
+
+
+	for (i = 0; str[i] != '\0'; i++)
+
+		if ((str[i] != del && str[i + 1] == del) ||
+		    (str[i] != del && !str[i + 1]) || str[i + 1] == del)
+			num_words++;
+	if (num_words == 0)
+		return (NULL);
+	s = malloc((1 + num_words) * sizeof(char *));
+	if (!s)
+		return (NULL);
+	for (i = 0, j = 0; j < num_words; j++)
+	{
+		while (str[i] == del && str[i] != del)
+			i++;
+		k = 0;
+		while (str[i + k] != del && str[i + k] && str[i + k] != del)
+			k++;
+		s[j] = malloc((k + 1) * sizeof(char));
+		if (!s[j])
+		{
+			for (k = 0; k < j; k++)
+				free(s[k]);
+			free(s);
+			return (NULL);
+		}
+		for (m = 0; m < k; m++)		s[j][m] = str[i++];
+		s[j][m] = 0;
+	}
+	s[j] = NULL;
+	return (s);
+}
