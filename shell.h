f85175687b4e25f0f7d20b67f8343b4c3f75fe7a
Raja El Morabet <rajaa.elmorabet@gmail.com>
2024-03-02 02:10:52 +0100

    add files liste and update shell.h

--- shell.h
+++ shell.h
@@ -62,4 +62,18 @@ char *convert_number(long int num, int base, int flags);
 void print_error(info_t *info, char *estr);
 int print_d(int input, int fd);
 
+/* Prototypes_file_lists1.c */
+list_t *add_node(list_t **, const char *, int);
+list_t *add_node_end(list_t **, const char *, int);
+size_t print_list_str(const list_t *);
+int delete_node_at_index(list_t **, unsigned int);
+void free_list(list_t **);
+
+/* Prototypes_file_lists2.c */
+size_t list_len(const list_t *);
+char **list_to_strings(list_t *);
+size_t print_list(const list_t *);
+list_t *node_starts_with(list_t *, char *, char);
+ssize_t get_node_index(list_t *, list_t *);
+
 #endif
