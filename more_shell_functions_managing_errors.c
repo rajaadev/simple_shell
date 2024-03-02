d302dd6d0ec9ac86be1513407e6830ad3eaccf1d
Raja El Morabet <rajaa.elmorabet@gmail.com>
2024-03-02 01:15:07 +0100

    update more_shell_functions_managing_errors.c

--- more_shell_functions_managing_errors.c
+++ more_shell_functions_managing_errors.c
@@ -1,4 +1,21 @@
 #include "shell.h"
+/**
+ * remove_comments - function that replaces first instance of '#' with '\0'
+ * @buf: string address to modify
+ * Return: 0
+ */
+void remove_comments(char *buf)
+{
+	int i;
+
+	for (i = 0; buf[i] != '\0'; i++)
+		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
+		{
+			buf[i] = '\0';
+			break;
+		}
+}
+
 
 /**
  * _erratoi - converts a string to an integer
@@ -36,5 +53,93 @@ int _erratoi(char *s)
     }
 
     return result * sign;
+}
+/**
+ * convert_number - converter function
+ * @number: number
+ * @base: base
+ * @flags: argument
+ * Return: string
+ */
+char *convert_number(long int number, int base, int flags)
+{
+	static char *array;
+	static char buffer[50];
+	char sign = 0;
+	char *ptr;
+	unsigned long n = number;
+
+	if (!(flags & CONVERT_UNSIGNED) && number < 0)
+	{
+		n = -number;
+		sign = '-';
+
+	}
+	array = flags & CONVERT_LOWERCASE ? "0123456789abcdef" : "0123456789ABCDEF";
+	ptr = &buffer[49];
+	*ptr = '\0';
+
+	do	{
+		*--ptr = array[n % base];
+		n /= base;
+	} while (n != 0);
+
+	if (sign)
+		*--ptr = sign;
+	return (ptr);
+}
+/**
+ * print_d - function that prints decimal integer number in base 10.
+ * @input: number to print
+ * @fd: the file descriptor to write to (stdout ou stderr)
+ * Return: number of characters printed
+ */
+int print_d(int input, int fd)
+{
+	int (*__putchar)(char) = _putchar;
+	int i, count = 0;
+	unsigned int _abs_, current;
+
+	if (fd == STDERR_FILENO)
+		__putchar = _eputchar;
+	if (input < 0)
+	{
+		_abs_ = -input;
+		__putchar('-');
+		count++;
+	}
+	else
+		_abs_ = input;
+	current = _abs_;
+	for (i = 1000000000; i > 1; i /= 10)
+	{
+		if (_abs_ / i)
+		{
+			__putchar('0' + current / i);
+			count++;
+		}
+		current %= i;
+	}
+	__putchar('0' + current);
+	count++;
+
+	return (count);
+}
+/**
+ * print_error - prints an error message
+ * @info:  pointer to info struct
+ * @estr: string containing specified error
+ * Return: 0 if no numbers in string, converted number otherwise
+ *        -1 on error
+ */
+void print_error(info_t *info, char *estr)
+{
+	_eputs(info->fname);
+	_eputs(": ");
+	print_d(info->line_count, STDERR_FILENO);
+	_eputs(": ");
+	_eputs(info->argv[0]);
+	_eputs(": ");
+	_eputs(estr);
 }
 
